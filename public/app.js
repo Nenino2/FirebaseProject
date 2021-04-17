2fa33989062ed2622ed52156f1c5bf7b42cf37bf
halbtale <halbtale@gmail.com>
2021-04-17 14:10:52 +0200

    Added

--- public/app.js
+++ public/app.js
@@ -1,10 +1,16 @@
 // @ts-check
 const db = firebase.default.firestore();
 
-async function getAllData() {
-    const { docs } = await db.collection('users').get();
+async function getAllData(collection) {
+    const { docs } = await db.collection(collection).get();
     const data = docs.map(doc => doc.data())
     console.log(data);
 }
 
-getAllData()
+async function setData(collection, documentData) {
+    await db.collection(collection).add(documentData)
+    console.log('ADDED DOCUMENT')
+}
+
+getAllData('users')
+setData('stonks', {rich: true})
